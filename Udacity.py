019fc4d7bec63837706795cd85a0e69c5c907d5a
Austin Slakey <austin.slakey@columbia.edu>
2015-11-09 12:27:01 -0500

    Update Udacity.py

--- Udacity.py
+++ Udacity.py
@@ -1,5 +1,6 @@
 import pandas
 import pandasql
+import csv
 
 
 def num_rainy_days(filename):
@@ -28,3 +29,33 @@ def num_rainy_days(filename):
     #Execute one of the SQL command against the pandas frame
     rainy_count = pandasql.sqldf(q1.lower(), locals())
     return rainy_count
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    In addition, here is a CSV reader/writer introductory tutorial:
+    http://goo.gl/HBbvyy
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file in the links below:
+    Sample input file:
+    https://www.dropbox.com/s/mpin5zv4hgrx244/turnstile_110528.txt
+    Sample updated file:
+    https://www.dropbox.com/s/074xbgio4c39b7h/solution_turnstile_110528.txt
+    '''
+    for name in filenames:
+        f_in = open(name,'r')
+        f_out = open("updated_"+name, 'w')
+
+        reader_in = csv.reader(f_in, delimiter=",")
+        writer_out = csv.writer(f_out,delimiter=",")
+        
+        # your code here
+        for line in reader_in:
+            first_el=line[0]
+            second_el=line[1]
+            third_el=line[2]
+            for element in range(0,len(line)):
+                if (element-2)%5==0:
+                    new_line = [first_el,second_el,third_el,line[element-4],line[element-3],line[element-2],line[element-1],line[element]]
+                    writer_out.writerow(new_line)
